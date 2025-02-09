5fc0ed12c4a81dc7bd7a7b516379e7b66b2f3b9c
Yaqoob-Aslam <reach.yaqoob@gmail.com>
2025-02-09 15:15:12 +0500

    Updated code

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter', // Add this line
-
+  retries: 3,
   e2e: {
     setupNodeEvents(on, config) {
       // Add this line to load the reporter plugin
